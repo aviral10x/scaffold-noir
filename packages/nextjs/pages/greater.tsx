05cd8aa7ea23d22d73352f09afe0c9897dc6b9d2
AVIRAL GUPTA <91749530+aviral10x@users.noreply.github.com>
2023-10-20 03:10:39 +0530

    greater

---
+++ packages/nextjs/pages/greater.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { CircuitUI } from "~~/components/noir";
+
+const greater = () => {
+  return (
+    <div>
+      <CircuitUI circuitName="greater" />
+    </div>
+  );
+};
+
+export default greater;
